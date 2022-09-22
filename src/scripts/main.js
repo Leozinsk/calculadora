175623c28add015a63c4dd52770a04e71eb20a1b
Leozinsk <leozinsk@gmail.com>
2022-09-22 04:55:25 +0100

    Calculadora 0.2
    redução de characteres em caso de muitos números, mas ainda nao foi limitado.
    Falta associação de operadores

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,33 @@
 const form = document.querySelector('.teclas')
 const visor = document.querySelector('.visor__info')
-const operadores = document.querySelectorAll('.operadores')
-
+const reg = /[0-9]/;
 
 form.addEventListener('click', (e)=>{
-    e.preventDefault();
-    console.log(e.target.innerText);
-    if(visor.innerHTML.length >= 8){
-        visor.classList.add('visor__small')
+e.preventDefault();
+    const targetTecla = e.target.value
+    
+    if(targetTecla){
+        visor.innerHTML += targetTecla
     }
-    visor.innerText += e.srcElement.innerText
+    console.log(typeof targetTecla )
+    console.log(e.target.value)
+
+    sizeChecking()
+
+    if(targetTecla === 'CE'|| targetTecla === 'C'){
+        visor.innerText = ''
+    }
+
 })
+
+function sizeChecking(){
+    if(visor.innerText.length >= 8){
+        visor.classList.add('visor__small')
+        if(visor.innerText.length >= 16){
+            visor.innerHTML
+        }
+    }else{
+        visor.classList.remove('visor__small')
+    }
+
+}
